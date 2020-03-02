b4f4f48f60fcd03a6f24172f65c076e7a0b1ccb0
Vasco Ramos <vascoalramos@outlook.pt>
2020-03-02 17:21:57 +0000

    :card_file_box: Added first database models

--- rest_api/models.py
+++ rest_api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class CustomUser(models.Model):
+    auth_user = models.OneToOneField(
+        User, on_delete=models.CASCADE, unique=True, primary_key=True
+    )
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    photo = models.TextField(null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+
+class Doctor(models.Model):
+    user = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE, unique=True, primary_key=True
+    )
