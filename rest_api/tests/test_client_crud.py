01cc8b92a8f668083a52ae0b93000b8d06d56e0c
Vasco Ramos <vascoalramos@outlook.pt>
2020-03-11 21:39:55 +0000

    :white_check_mark: Add last tests of get client info endpoint

--- rest_api/tests/test_client_crud.py
+++ rest_api/tests/test_client_crud.py
@@ -97,28 +97,37 @@ class GetClientTest(APITestCase):
         self.client.post("/clients",
                          {"email": "tos@ua.pt", "password": "pwd", "first_name": "Tomas",
                           "last_name": "Ramos", "height": 1.60, "weight_goal": 65})
-
-    def test_get_client_info_client_no_doctor(self):
         auth_user = User.objects.create_user("ana@ua.pt", "ana@ua.pt", "pwd")
         user = CustomUser.objects.create(auth_user=auth_user, birth_date=date(2020, 12, 31))
-        doctor = Doctor.objects.create(user=user, hospital="Hospital")
+        self.doctor = Doctor.objects.create(user=user, hospital="Hospital")
         doctors_group = Group.objects.get_or_create(name="doctors_group")[0]
         doctors_group.user_set.add(auth_user)
-        response = self.client.post("/login", {"username": "ana@ua.pt", "password": "pwd"})
+
+    def login(self, username, pwd):
+        response = self.client.post("/login", {"username": username, "password": pwd})
         token = response.data["token"]
         self.client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
+
+    def test_get_client_info_other_client(self):
+        self.client.post("/clients",
+                         {"email": "vr@ua.pt", "password": "pwd", "first_name": "Tomas",
+                          "last_name": "Ramos", "height": 1.60, "weight_goal": 65})
+        self.login("vr@ua.pt", "pwd")
         response = self.client.get("/clients/tos@ua.pt")
         self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
 
+    def test_get_client_info_client_no_doctor(self):
+        self.login("ana@ua.pt", "pwd")
+        response = self.client.get("/clients/tos@ua.pt")
+        self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
+
+    def test_get_client_self_info(self):
+        self.login("tos@ua.pt", "pwd")
+        response = self.client.get("/clients/tos@ua.pt")
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
     def test_get_client_info_client_doctor(self):
-        auth_user = User.objects.create_user("ana@ua.pt", "ana@ua.pt", "pwd")
-        user = CustomUser.objects.create(auth_user=auth_user, birth_date=date(2020, 12, 31))
-        doctor = Doctor.objects.create(user=user, hospital="Hospital")
-        doctors_group = Group.objects.get_or_create(name="doctors_group")[0]
-        doctors_group.user_set.add(auth_user)
-        Client.objects.filter(user__auth_user__username="tos@ua.pt").update(doctor=doctor)
-        response = self.client.post("/login", {"username": "ana@ua.pt", "password": "pwd"})
-        token = response.data["token"]
-        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
+        Client.objects.filter(user__auth_user__username="tos@ua.pt").update(doctor=self.doctor)
+        self.login("ana@ua.pt", "pwd")
         response = self.client.get("/clients/tos@ua.pt")
         self.assertEqual(response.status_code, HTTP_200_OK)
