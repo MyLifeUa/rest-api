ecf9b28145a5ea1f884ea1be1f744026aaf3392f
Vasco Ramos <vascoalramos@outlook.pt>
2020-03-11 10:04:07 +0000

    :white_check_mark: Cleaning up tests code

--- rest_api/tests/test_admin_crud.py
+++ rest_api/tests/test_admin_crud.py
@@ -4,8 +4,20 @@ from rest_framework.test import APITestCase
 
 
 class AdminRegistrationTest(APITestCase):
-    def login(self):
-        return self.client.post("/login", {"username": "vasco", "password": "pwd"})
+    def login(self, username, password):
+        return self.client.post("/login", {"username": username, "password": password})
+
+    def create_user_and_login(self, role, username, email, password):
+        if role == "client":
+            user = User.objects.create_user(username, email, password)
+            clients_group = Group.objects.get_or_create(name="clients_group")[0]
+            clients_group.user_set.add(user)
+        elif role == "admin":
+            User.objects.create_superuser(username, email, password)
+        response = self.login("vasco", "pwd")
+        token = response.data["token"]
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
+
 
     def test_new_admin_missing_authentication(self):
         response = self.client.post("/admins", {"email": "vr@ua.pt", "password": "pwd", "first_name": "Vasco",
@@ -13,30 +25,19 @@ class AdminRegistrationTest(APITestCase):
         self.assertEqual(response.status_code, HTTP_401_UNAUTHORIZED)
 
     def test_new_admin_missing_authorization(self):
-        user = User.objects.create_user("vasco", "vr@ua.pt", "pwd")
-        clients_group = Group.objects.get_or_create(name="clients_group")[0]
-        clients_group.user_set.add(user)
-        response = self.login()
-        token = response.data["token"]
-        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
+        self.create_user_and_login("client", "vasco", "vr@ua.pt", "pwd")
         response = self.client.post("/admins", {"email": "vr@ua.pt", "password": "pwd", "first_name": "Vasco",
                                                 "last_name": "Ramos", "hospital": "Centro Hospitalar de São João"})
         self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
 
     def test_new_admin_missing_parameters(self):
-        User.objects.create_superuser("vasco", "vr@ua.pt", "pwd")
-        response = self.login()
-        token = response.data["token"]
-        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
+        self.create_user_and_login("admin", "vasco", "vr@ua.pt", "pwd")
         response = self.client.post("/admins", {"email": "vr@ua.pt", "password": "pwd", "first_name": "Vasco",
                                                 "last_name": "Ramos"})
         self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
 
     def test_new_admin_right_parameters(self):
-        User.objects.create_superuser("vasco", "vr@ua.pt", "pwd")
-        response = self.login()
-        token = response.data["token"]
-        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
+        self.create_user_and_login("admin", "vasco", "vr@ua.pt", "pwd")
         response = self.client.post("/admins", {"email": "vr@ua.pt", "password": "pwd", "first_name": "Vasco",
                                                 "last_name": "Ramos", "hospital": "Centro Hospitalar de São João"})
         self.assertEqual(response.status_code, HTTP_200_OK)
