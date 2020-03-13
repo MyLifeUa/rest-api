128411522a050ad09e2c89459b11a306fe70663b
Vasco Ramos <vascoalramos@outlook.pt>
2020-03-13 16:42:48 +0000

    :recycle: Refactored code in queries.py

--- rest_api/queries.py
+++ rest_api/queries.py
@@ -6,35 +6,99 @@ from .constants import *
 from .serializers import ClientSerializer, DoctorSerializer
 
 
-def add_client(data):
+def add_user(data, is_superuser=False):
     email = data.get("email")
     first_name = data.get("first_name")
     last_name = data.get("last_name")
     password = data.get("password")
-    height = data.get("height")
-    weight_goal = data.get("weight_goal")
-    birth_date = data.get("birth_date")
 
-    # treat nullable fields
-    phone_number = data.get("phone_number") if "phone_number" in data else None
-    photo = data.get("photo") if "photo" in data else DEFAULT_USER_IMAGE
+    if not is_superuser:
+        birth_date = data.get("birth_date")
+
+        # treat nullable fields
+        phone_number = data.get("phone_number") if "phone_number" in data else None
+        photo = data.get("photo") if "photo" in data else DEFAULT_USER_IMAGE
 
     if User.objects.filter(username=email).exists():
         error_message = "Email already taken. User was not added to the db."
         return False, error_message
 
     try:
-        # create a user
-        user = User.objects.create_user(username=email, email=email, first_name=first_name, last_name=last_name,
-                                        password=password)
+        if is_superuser:
+            # create a user
+            user = User.objects.create_superuser(username=email, email=email, first_name=first_name,
+                                                 last_name=last_name, password=password)
 
-        # create custom user
-        custom_user = CustomUser.objects.create(auth_user=user, phone_number=phone_number, photo=photo,
-                                                birth_date=birth_date)
+        else:
+            # create a user
+            auth_user = User.objects.create_user(username=email, email=email, first_name=first_name,
+                                                 last_name=last_name, password=password)
+
+            # create custom user
+            user = CustomUser.objects.create(auth_user=auth_user, phone_number=phone_number, photo=photo,
+                                             birth_date=birth_date)
 
     except Error:
         error_message = "Error while creating new user!"
         return False, error_message
+
+    return True, user
+
+
+def update_user(data, auth_user, user):
+    if "email" in data:
+        email = data.get("email")
+        auth_user.update(email=email)
+        auth_user.update(username=email)
+
+    if "first_name" in data:
+        first_name = data.get("first_name")
+        auth_user.update(first_name=first_name)
+
+    if "last_name" in data:
+        last_name = data.get("last_name")
+        auth_user.update(last_name=last_name)
+
+    if "password" in data:
+        user = User.objects.get(username=email)
+        auth_user.set_password(data.get("password"))
+        auth_user.save()
+
+    if "phone_number" in data:
+        phone_number = data.get("phone_number")
+        user.update(phone_number=phone_number)
+
+    if "photo" in data:
+        photo = data.get("photo")
+        user.update(photo=photo)
+
+    if "birth_date" in data:
+        birth_date = data.get("birth_date")
+        user.update(birth_date=birth_date)
+
+
+def delete_user(user):
+    try:
+        user.delete()
+        state, message = True, "User successfully deleted"
+
+    except Error:
+        state, message = False, "Error while deleting user"
+
+    finally:
+        return state, message
+
+
+def add_client(data):
+    height = data.get("height")
+    weight_goal = data.get("weight_goal")
+
+    state, content = add_user(data)
+    if not state:
+        return state, content
+
+    custom_user = content
+    user = custom_user.auth_user
 
     try:
         # link the user to a client
@@ -62,53 +126,19 @@ def add_client(data):
 
 def update_client(request, email):
     data = request.data
-    state, message = None, None
+    state = True
+    message = "Client successfully updated!"
 
-    auth_user = User.objects.filter(username=email)
-    if not auth_user.exists():
-        state, message = False, "User does not exist!"
-        return state, message
-
-    user = CustomUser.objects.filter(auth_user=auth_user[0])
-    if not user.exists():
-        state, message = False, "User does not exist!"
-        return state, message
-
-    client = Client.objects.filter(user=user[0])
+    client = Client.objects.filter(user__auth_user__username=email)
     if not client.exists():
-        state, message = False, "User is not a client!"
+        state, message = False, "User does not exist or user is not a client!"
         return state, message
 
     try:
-        if "email" in data:
-            email = data.get("email")
-            auth_user.update(email=email)
-            auth_user.update(username=email)
+        auth_user = User.objects.filter(username=email)
+        user = CustomUser.objects.filter(auth_user=auth_user[0])
 
-        if "first_name" in data:
-            first_name = data.get("first_name")
-            auth_user.update(first_name=first_name)
-
-        if "last_name" in data:
-            last_name = data.get("last_name")
-            auth_user.update(last_name=last_name)
-
-        if "password" in data:
-            user = User.objects.get(username=email)
-            auth_user.set_password(data.get("password"))
-            auth_user.save()
-
-        if "phone_number" in data:
-            phone_number = data.get("phone_number")
-            user.update(phone_number=phone_number)
-
-        if "photo" in data:
-            photo = data.get("photo")
-            user.update(photo=photo)
-
-        if "birth_date" in data:
-            birth_date = data.get("birth_date")
-            user.update(birth_date=birth_date)
+        update_user(data, auth_user, user)
 
         if "height" in data:
             height = data.get("height")
@@ -118,10 +148,6 @@ def update_client(request, email):
             weight_goal = data.get("weight_goal")
             client.update(weight_goal=weight_goal)
 
-        if state is None:
-            state = True
-            message = "Client successfully updated!"
-
     except Exception:
         state, message = False, "Error while updating client!"
 
@@ -129,44 +155,23 @@ def update_client(request, email):
 
 
 def get_client(email):
-    user = CustomUser.objects.filter(auth_user__username=email)
-    if not user.exists():
-        state, message = False, "User does not exist!"
-        return state, message
-
-    client = Client.objects.filter(user=user[0])
+    client = Client.objects.filter(user__auth_user__username=email)
     if not client.exists():
-        state, message = False, "User is not a client!"
+        state, message = False, "User does not exist or user is not a client!"
         return state, message
 
-    state, message = True, ClientSerializer(Client.objects.get(user=user[0])).data
+    state, message = True, ClientSerializer(client[0]).data
     return state, message
 
 
 def add_admin(data):
-    email = data.get("email")
-    first_name = data.get("first_name")
-    last_name = data.get("last_name")
-    password = data.get("password")
     hospital = data.get("hospital")
 
-    if User.objects.filter(username=email).exists():
-        error_message = "Email already taken. User was not added to the db."
-        return False, error_message
+    state, content = add_user(data, is_superuser=True)
+    if not state:
+        return state, content
 
-    try:
-        # create a user
-        user = User.objects.create_superuser(
-            username=email,
-            email=email,
-            first_name=first_name,
-            last_name=last_name,
-            password=password,
-        )
-
-    except Error:
-        error_message = "Error while creating new user!"
-        return False, error_message
+    user = content
 
     try:
         # link the user to an admin
@@ -181,49 +186,13 @@ def add_admin(data):
     return True, state_message
 
 
-def delete_user(user):
-    try:
-        user.delete()
-        state, message = True, "User successfully deleted"
-
-    except Error:
-        state, message = False, "Error while deleting user"
-
-    finally:
-        return state, message
-
-
 def add_doctor(data, hospital):
-    email = data.get("email")
-    first_name = data.get("first_name")
-    last_name = data.get("last_name")
-    password = data.get("password")
-    birth_date = data.get("birth_date")
+    state, content = add_user(data)
+    if not state:
+        return state, content
 
-    # treat nullable fields
-    phone_number = data.get("phone_number") if "phone_number" in data else None
-    photo = data.get("photo") if "photo" in data else DEFAULT_USER_IMAGE
-
-    if User.objects.filter(username=email).exists():
-        error_message = "Email already taken. User was not added to the db."
-        return False, error_message
-
-    try:
-        # create a user
-        user = User.objects.create_user(username=email, email=email, first_name=first_name, last_name=last_name,
-                                        password=password)
-
-    except Error:
-        error_message = "Error while creating new user!"
-        return False, error_message
-
-    try:
-        custom_user = CustomUser.objects.create(auth_user=user, phone_number=phone_number, photo=photo,
-                                                birth_date=birth_date)
-
-    except Error:
-        error_message = "Error while creating new custom_user!"
-        return False, error_message
+    custom_user = content
+    user = custom_user.auth_user
 
     try:
         # link the user to a doctor
@@ -235,7 +204,7 @@ def add_doctor(data, hospital):
         return False, error_message
 
     # check if the doctor group exists, else create it
-    # finally add client to group
+    # finally add doctor to group
     try:
         doctors_group, created = Group.objects.get_or_create(name="doctors_group")
         doctors_group.user_set.add(user)
@@ -251,57 +220,19 @@ def add_doctor(data, hospital):
 
 def update_doctor(request, email):
     data = request.data
-    state, message = None, None
+    state = True
+    message = "Doctor successfully updated!"
 
-    auth_user = User.objects.filter(username=email)
-    if not auth_user.exists():
-        state, message = False, "User does not exist!"
-        return state, message
-
-    user = CustomUser.objects.filter(auth_user=auth_user[0])
-    if not user.exists():
-        state, message = False, "User does not exist!"
-        return state, message
-
-    doctor = Doctor.objects.filter(user=user[0])
+    doctor = Doctor.objects.filter(user__auth_user__username=email)
     if not doctor.exists():
-        state, message = False, "User is not a doctor!"
+        state, message = False, "User does not exist or user is not a doctor!"
         return state, message
 
     try:
-        if "email" in data:
-            email = data.get("email")
-            auth_user.update(email=email)
-            auth_user.update(username=email)
+        auth_user = User.objects.filter(username=email)
+        user = CustomUser.objects.filter(auth_user=auth_user[0])
 
-        if "first_name" in data:
-            first_name = data.get("first_name")
-            auth_user.update(first_name=first_name)
-
-        if "last_name" in data:
-            last_name = data.get("last_name")
-            auth_user.update(last_name=last_name)
-
-        if "password" in data:
-            user = User.objects.get(username=email)
-            auth_user.set_password(data.get("password"))
-            auth_user.save()
-
-        if "phone_number" in data:
-            phone_number = data.get("phone_number")
-            user.update(phone_number=phone_number)
-
-        if "photo" in data:
-            photo = data.get("photo")
-            user.update(photo=photo)
-
-        if "birth_date" in data:
-            birth_date = data.get("birth_date")
-            user.update(birth_date=birth_date)
-
-        if state is None:
-            state = True
-            message = "Doctor successfully updated!"
+        update_user(data, auth_user, user)
 
     except Exception:
         state, message = False, "Error while updating client!"
@@ -310,12 +241,10 @@ def update_doctor(request, email):
 
 
 def get_doctor(email):
-    user = CustomUser.objects.filter(auth_user__username=email)
-    if not user.exists():
-        state, message = False, "User does not exist!"
+    doctor = Doctor.objects.filter(user__auth_user__username=email)
+    if not doctor.exists():
+        state, message = False, "User does not exist or user is not a doctor!"
         return state, message
-
-    doctor = Doctor.objects.filter(user=user[0])
 
     state, message = True, DoctorSerializer(doctor[0]).data
     return state, message
