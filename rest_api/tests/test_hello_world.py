328193ca55c1b4f96d5ef8b9d75aa45b4f415233
Vasco Ramos <vascoalramos@outlook.pt>
2020-03-30 19:49:56 +0100

    ✨ Added hello world and tests

--- rest_api/tests/test_hello_world.py
+++ rest_api/tests/test_hello_world.py
@@ -4,7 +4,7 @@ from rest_framework.status import (
     HTTP_405_METHOD_NOT_ALLOWED
 )
 
-'''
+
 class HelloWorldTest(APITestCase):
     def test_hello_world_wrong_request_type(self):
         response = self.client.post("/hello-world")
@@ -13,4 +13,3 @@ class HelloWorldTest(APITestCase):
     def test_hello_world_correct_request_type(self):
         response = self.client.get("/hello-world")
         self.assertEqual(response.status_code, HTTP_200_OK)
-'''
