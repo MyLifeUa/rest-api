c42170c605890de83dbaf511935bba23c7e471ca
joao-p-marques <joao.marques.contacto@gmail.com>
2020-04-20 16:57:43 +0100

    rm comments

--- rest_api/queries.py
+++ rest_api/queries.py
@@ -353,8 +353,6 @@ def get_food_log(email, day):
 
     meal_history = MealHistory.objects.filter(day=day, client=current_client)
 
-    # TODO: group meal history by moments on day. return goal and consumed calories (for each meal also)
-
     data = group_meals(meal_history, current_client)
 
     # state, message = True, [MealHistorySerializer(r).data for r in meal_history]
