d99ea5478caa0087109b896451d9f9428e22c689
Vasco Ramos <vascoalramos@outlook.pt>
2020-04-22 10:32:28 +0100

    🍻 Just to tests

--- rest_api/queries.py
+++ rest_api/queries.py
@@ -234,10 +234,14 @@ def get_client(email):
         fitbit_access_token = client.fitbit_access_token
         fitbit_refresh_token = client.fitbit_refresh_token
 
+        print(fitbit_access_token)
+
         if fitbit_access_token is not None and fitbit_refresh_token is not None:
             fitbit_api = fitbit.Fitbit(CLIENT_FITBIT_ID, CLIENT_FITBIT_SECRET, system="en_UK", oauth2=True,
                                        access_token=fitbit_access_token, refresh_token=fitbit_refresh_token,
                                        refresh_cb=client.refresh_cb)
+
+            print(fitbit_access_token)
 
             message["steps"] = fitbit_api.time_series("activities/steps", period="1d")["activities-steps"][0]["value"]
             message["distance"] = fitbit_api.time_series("activities/distance", period="1d")["activities-distance"][0][
@@ -252,7 +256,8 @@ def get_client(email):
         state = False
         message = "User does not exist or user is not a client!"
 
-    except Exception:
+    except Exception as e:
+        print(e)
         state = False
         message = "Error while trying to fetch client information"
 
