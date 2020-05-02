162d611fa9c4046603f2e5736c50c62cfff9a30a
Vasco Ramos <vascoalramos@outlook.pt>
2020-05-02 18:26:55 +0100

    ♻️ Update returning codes

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -56,7 +56,8 @@ urlpatterns = [
     url("^health-stats/nutrients/total/(?P<email>.+)/(?P<date>.+)", nutrients_total, name="nutrients-total"),
     url("^health-stats/nutrients/history/(?P<email>.+)", nutrients_history, name="nutrients-history"),
     url("^health-stats/body/history/(?P<email>.+)", body_history, name="body-history"),
-    url("^health-stats/body/heart-rate/(?P<email>.+)", body_avg_heart_rate, name="body-history"),
+    url("^health-stats/body/heart-rate/(?P<email>.+)", body_avg_heart_rate, name="body-avg_heart_rate"),
+    url("^health-stats/body/my-life/(?P<email>.+)", my_life_metric, name="my-life-metric"),
 
     # Expo Tokens
     path("expo-tokens", expo_tokens_post_and_get, name="expo-tokens-get-and-post"),
