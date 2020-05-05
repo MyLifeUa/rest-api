116343f4e76f66abfe7d084731fc451dfcfaa7cb
Tiago Mendes <tiagocmendes@ua.pt>
2020-05-05 16:32:00 +0100

    Add logs

--- cronjobs/cronjob.py
+++ cronjobs/cronjob.py
@@ -1,8 +1,30 @@
+import logging
+from datetime import datetime
 from driver import PostgresDriver
 
+logging.basicConfig(level=logging.DEBUG, filename='cronjob.log', filemode='w', format='%(asctime)s %(levelname)s:%(message)s')
+
 if __name__ == '__main__':
+
+    # start cronjob
+    logging.info("Start cronjob")
+
+    # today date
+    today = datetime.today().strftime("%Y-%m-%d")
+    logging.debug(f"Today date: {today}")
+
+    # database connection
     dolphin = PostgresDriver()
+    logging.info(f"Database connected with success on {dolphin.host}:{dolphin.port}!")
 
-    clients = dolphin.select_all("SELECT * FROM rest_api_client")
+    logging.debug("SELECT * FROM rest_api_client;")
+    clients = dolphin.select_all("SELECT * FROM rest_api_client;")
+    logging.debug(f"Query result: \n{clients}")
 
-    print(clients)
+    for client in clients:
+        client_id = int(client[0])
+        logging.debug(f"SELECT * FROM rest_api_mealhistory WHERE client={client_id};")
+        meals = dolphin.select_all(f"SELECT * FROM rest_api_mealhistory WHERE client={client_id};")
+        logging.debug(f"Query result: \n{meals}")
+
+    
