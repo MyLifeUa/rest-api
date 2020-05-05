14c8030a5148b60a8f1fe72eac104db05a01e619
Tiago Mendes <tiagocmendes@ua.pt>
2020-05-05 16:46:19 +0100

    Select meal history

--- cronjobs/cronjob.py
+++ cronjobs/cronjob.py
@@ -18,13 +18,11 @@ if __name__ == '__main__':
     logging.info(f"Database connected with success on {dolphin.host}:{dolphin.port}!")
 
     logging.debug("SELECT * FROM rest_api_client;")
-    clients = dolphin.select_all("SELECT * FROM rest_api_client;")
+    clients = sorted(dolphin.select_all("SELECT * FROM rest_api_client;"))
     logging.debug(f"Query result: \n{clients}")
 
     for client in clients:
         client_id = int(client[0])
         logging.debug(f"SELECT * FROM rest_api_mealhistory WHERE client={client_id};")
-        meals = dolphin.select_all(f"SELECT * FROM rest_api_mealhistory WHERE client={client_id};")
+        meals = dolphin.select_all(f"SELECT * FROM rest_api_mealhistory WHERE rest_api_mealhistory.client_id={client_id} AND rest_api_mealhistory.day='{today}';")
         logging.debug(f"Query result: \n{meals}")
-
-    
